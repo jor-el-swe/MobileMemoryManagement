5170955000ff4f2c27240d09f7cc657ccf6566c1
Joel Ring <joel.ring@gmail.com>
2021-02-15 17:54:26 +0100

    adds scene changer version

---
+++ MobileMemoryManagement3/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    private void Start(){
+        //optimization 1:
+        //Resources.UnloadUnusedAssets();
+        
+        //optimization 2:
+        //Resources.UnloadUnusedAssets();
+        //GC.Collect();
+    }
+
+    public void nextScene(){
+        var currentBuildIndex = SceneManager.GetActiveScene().buildIndex;
+        currentBuildIndex++;
+        if (currentBuildIndex >= SceneManager.sceneCountInBuildSettings)
+            currentBuildIndex = 0;
+        
+        //optimization 1:
+        //Resources.UnloadUnusedAssets();
+        
+        //optimization 2:
+        //Resources.UnloadUnusedAssets();
+        //GC.Collect();
+        
+        SceneManager.LoadScene(currentBuildIndex);
+    }
+}
