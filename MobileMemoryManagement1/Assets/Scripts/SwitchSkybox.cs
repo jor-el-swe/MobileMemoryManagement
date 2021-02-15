b67f49dfbea8b7a8dbeed719623d304aaf297fe2
Joel Ring <joel.ring@gmail.com>
2021-02-15 11:32:38 +0100

    implements switching of skyboxes

---
+++ MobileMemoryManagement1/Assets/Scripts/SwitchSkybox.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SwitchSkybox : MonoBehaviour{
+    public Material[] skyboxMaterials = new Material[6];
+    private Skybox skybox;
+    private static int skyboxIndex = 0;
+    
+    // Start is called before the first frame update
+    void Start(){
+        skybox = GetComponent<Skybox>();
+    }
+    
+    public void NextSkybox(){
+        skyboxIndex++;
+        if ((skyboxIndex + 1) > skyboxMaterials.Length){
+            skyboxIndex = 0;
+        }
+        skybox.material = skyboxMaterials[skyboxIndex];
+    }
+}
