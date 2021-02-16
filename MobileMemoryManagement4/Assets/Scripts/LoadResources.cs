6904869e6fb8f820f46e53074b6ef9b890912238
Joel Ring <joel.ring@gmail.com>
2021-02-16 11:24:58 +0100

    adds instantiate for materials

--- MobileMemoryManagement4/Assets/Scripts/LoadResources.cs
+++ MobileMemoryManagement4/Assets/Scripts/LoadResources.cs
@@ -1,28 +1,36 @@
-using System;
 using UnityEngine;
 
 public class LoadResources : MonoBehaviour{
     private static int skyboxIndex = 0;
+    private string loadPath = "skybox";
+    private Skybox skybox;
     
-    void Start()
-    {
-        //load the first skybox
+    void Start(){
+        skybox = Camera.main.GetComponent<Skybox>();
         
+        //load the first skybox
+        var material = Resources.Load<Material>(loadPath + "0");
+        skybox.material = Instantiate(material);
     }
     
     
     public void LoadNextSkybox(){
+        Destroy(skybox.material);
+        
         skyboxIndex++;
         if (skyboxIndex >= 6){
             skyboxIndex = 0;
         }
       
+        //load the next skybox material
+        var material = Resources.Load<Material>(loadPath + skyboxIndex);
+        skybox.material = Instantiate(material);
         
         //optimization 1:
-        //Resources.UnloadUnusedAssets();
+        Resources.UnloadUnusedAssets();
         
         //optimization 2:
-        Resources.UnloadUnusedAssets();
-        GC.Collect();
+        //Resources.UnloadUnusedAssets();
+        //GC.Collect();
     }
 }
